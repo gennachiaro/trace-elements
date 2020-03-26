3f88db6d7afefb1e9bfeab81c8a5a6c4107510e0
gennachiaro <gennachiaro@gmail.com>
2020-03-26 15:27:05 -0500

    ok

--- trace-elements-hsr.py
+++ trace-elements-hsr.py
@@ -38,21 +38,20 @@ sns.set_style("darkgrid")
 #sns.set_palette("PuBuGn_d")
 
 #create plot
-plot = sns.scatterplot(data = MG, x= 'La', y='Ti',hue = "Population" , style = MG_index, palette="Blues_d",marker = 's', edgecolor="black", s=150,alpha = 0.5, legend = "brief")
-plot = sns.scatterplot(data = VCCR, x= 'La', y='Ti',hue = "Population", palette="PuRd_d", marker = '^', edgecolor="black", s=150, style = VCCR_index, alpha = 0.5, hue_order = ['VCCR 1', 'VCCR 2', 'VCCR 3'], legend = "brief")
+plot = sns.scatterplot(data = MG, x= 'Ba', y='Sr',hue = "Population" , style = MG_index, palette="Blues_d",marker = 's', edgecolor="black", s=150,alpha = 0.5, legend = "brief")
+plot = sns.scatterplot(data = VCCR, x= 'Ba', y='Sr',hue = "Population", palette="PuRd_d", marker = '^', edgecolor="black", s=150, style = VCCR_index, alpha = 0.5, hue_order = ['VCCR 1', 'VCCR 2', 'VCCR 3'], legend = "brief")
 
 #plot = sns.scatterplot(data = FG, x= 'Y', y='Nb',hue = FG_index, palette="Blues",legend="brief", marker = 's', edgecolor="black", s=150)
 #plot = sns.scatterplot(data = FGCP, x= 'Y', y='Nb',hue = FGCP_index, palette="Blues",legend="brief", marker = 's', edgecolor="black", s=150)
 
 #set y axis to log scale
-#plot.set(yscale='log')
-#plot.set(xscale='log')
+plot.set(yscale='log')
+plot.set(xscale='log')
 
 #set location of legend
 
 #plt.legend(loc='upper left')
 plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), ncol=2)
-
 
 # general title
 plt.suptitle("High-Silica Rhyolite (MG + VCCR) Fiamme Glass", fontsize=15, fontweight=0, color='black')
