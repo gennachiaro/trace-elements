75061b23a2e4cc24063a589fe208451eb86cb721
gennachiaro <gennachiaro@gmail.com>
2020-03-29 18:52:56 -0500

    ok

--- vccr-q2f-filtered.py
+++ vccr-q2f-filtered.py
@@ -13,7 +13,8 @@ import matplotlib.pyplot as plt
 #import csv file
 df = pd.read_csv('/Users/gennachiaro/Documents/vanderbilt/research/ora caldera/trace-elements/TraceElements_All.csv', index_col=1)
 
-VCCR = df.loc [['ORA-5B-402','ORA-5B-404A','ORA-5B-404B','ORA-5B-405','ORA-5B-406','ORA-5B-407','ORA-5B-408-SITE2','ORA-5B-408-SITE7','ORA-5B-408-SITE8','ORA-5B-409','ORA-5B-411','ORA-5B-412A-CG','ORA-5B-412B-CG','ORA-5B-413','ORA-5B-414-CG','ORA-5B-415','ORA-5B-416','ORA-5B-417']]
+#VCCR = df.loc [['ORA-5B-402','ORA-5B-404A','ORA-5B-404B','ORA-5B-405','ORA-5B-406','ORA-5B-407','ORA-5B-408-SITE2','ORA-5B-408-SITE7','ORA-5B-408-SITE8','ORA-5B-409','ORA-5B-411','ORA-5B-412A-CG','ORA-5B-412B-CG','ORA-5B-413','ORA-5B-414-CG','ORA-5B-415','ORA-5B-416','ORA-5B-417']]
+VCCR = df.loc [['ORA-5B-404A','ORA-5B-404B','ORA-5B-406','ORA-5B-408-SITE2','ORA-5B-408-SITE7','ORA-5B-408-SITE8','ORA-5B-411', 'ORA-5B-416']]
 VCCR_index = VCCR.index
 
 #set background color
@@ -22,14 +23,11 @@ sns.set_style("darkgrid")
 #plt.ylim(10, 50)
 #plt.xlim (25,100)
 
-# create color palette
-#flatui= ["#A4D3EE", "#4682B4", "#104E8B","#FFE4E1", "#FFB5C5", "#CD6090"]
-
 # set color palette
 #sns.set_palette("PuBuGn_d")
 
 #create plot
-plot = sns.scatterplot(data = VCCR, x= 'Ba', y='Sr',hue = "Population", palette="PuRd_d", marker = '^', edgecolor="black", s=150, legend = "brief", alpha = 0.5, hue_order = ['VCCR 1', 'VCCR 2', 'VCCR 3'])
+plot = sns.scatterplot(data = VCCR, x= 'Ba', y='Sr',hue = "Population",  style= VCCR_index, palette="PuRd_d", marker = '^', edgecolor="black", s=150, legend = "brief", alpha = 0.5, hue_order = ['VCCR 1', 'VCCR 2', 'VCCR 3'])
 
 #set y axis to log scale
 #plot.set(yscale='log')
