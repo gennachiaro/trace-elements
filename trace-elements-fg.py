a59b470cfe0fb72e1f8e3cac5c3fd0d4b7b24e44
gennachiaro <gennachiaro@gmail.com>
2020-04-09 13:39:00 -0500

    ok

--- trace-elements-fg.py
+++ trace-elements-fg.py
@@ -11,7 +11,7 @@ import seaborn as sns; sns.set()
 import matplotlib.pyplot as plt
 
 #import csv file
-df = pd.read_csv('/Users/gennachiaro/Documents/vanderbilt/research/ora caldera/trace-elements/TraceElements_All_TEMP.csv', index_col=1)
+df = pd.read_csv('/Users/gennachiaro/Documents/vanderbilt/research/ora caldera/trace-elements/TraceElements_All.csv', index_col=1)
 
 #FGCP = df.loc[['ORA-2A-002_Type1','ORA-2A-002_Type2','ORA-2A-002','ORA-2A-003','ORA-2A-016_Type1','ORA-2A-016-Type2','ORA-2A-016-Type3','ORA-2A-016-Type4','ORA-2A-023','ORA-2A-024','MINGLED1-ORA-2A-024','MINGLED2-ORA-2A-024','MINGLED3-ORA-2A-024']]
 #MG = df.loc[['ORA-2A-001','ORA-2A-005','ORA-2A-018','ORA-2A-031','ORA-2A-032','ORA-2A-035','ORA-2A-040']]
@@ -27,7 +27,7 @@ sns.set_style("darkgrid")
 #plt.xlim (25,100)
 
 #create plot
-plot = sns.scatterplot(data = FG, x= 'Ba', y='Sr', style = FG_index, hue = "Population", palette="binary_r", edgecolor="black", s=100,alpha = 0.5,legend="brief", markers=('o', 's', '^', 'X'),hue_order = ['FG 1', 'FG 2', 'FG 3'])
+plot = sns.scatterplot(data = FG, x= 'Ba', y='Sr', style = FG_index, hue = "Population", palette="OrRd_r", edgecolor="black", s=150,alpha = 0.5,legend="brief",hue_order = ['FG 1', 'FG 2', 'FG 3'])
 
 #set y axis to log scale
 #plot.set(yscale='log')
