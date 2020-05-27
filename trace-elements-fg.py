c546a1113204edfae709adf914cc4640bba02432
gennachiaro <gennachiaro@gmail.com>
2020-05-27 00:12:53 -0500

    ok

--- trace-elements-fg.py
+++ trace-elements-fg.py
@@ -16,15 +16,15 @@ df = pd.read_csv('/Users/gennachiaro/Documents/vanderbilt/research/ora caldera/t
 #FGCP = df.loc[['ORA-2A-002_Type1','ORA-2A-002_Type2','ORA-2A-002','ORA-2A-003','ORA-2A-016_Type1','ORA-2A-016-Type2','ORA-2A-016-Type3','ORA-2A-016-Type4','ORA-2A-023','ORA-2A-024','MINGLED1-ORA-2A-024','MINGLED2-ORA-2A-024','MINGLED3-ORA-2A-024']]
 #MG = df.loc[['ORA-2A-001','ORA-2A-005','ORA-2A-018','ORA-2A-031','ORA-2A-032','ORA-2A-035','ORA-2A-040']]
 #VCCR = df.loc [['ORA-5B-402','ORA-5B-404A','ORA-5B-404B','ORA-5B-405','ORA-5B-406','ORA-5B-407','ORA-5B-408-SITE2','ORA-5B-408-SITE7','ORA-5B-408-SITE8','ORA-5B-409','ORA-5B-411','ORA-5B-412A-CG','ORA-5B-412B-CG','ORA-5B-413','ORA-5B-414-CG','ORA-5B-415','ORA-5B-416','ORA-5B-417']]
-FG = df.loc [['ORA-5B-410','ORA-5B-412-FG','ORA-5B-414-FG']]
+FG = df.loc [['ORA-5B-410-FG','ORA-5B-412-FG','ORA-5B-414-FG']]
 FG_index = FG.index
 
 #set background color
 sns.set_style("darkgrid")
 
 
-#plt.ylim(10, 50)
-#plt.xlim (25,100)
+plt.ylim(0,300,50)
+plt.xlim (0,300,50)
 
 #create plot
 plot = sns.scatterplot(data = FG, x= 'Ba', y='Sr', style = FG_index, hue = "Population", palette="OrRd_r", edgecolor="black", s=150,alpha = 0.5,legend="brief",hue_order = ['FG 1', 'FG 2', 'FG 3'])
@@ -36,7 +36,7 @@ plot = sns.scatterplot(data = FG, x= 'Ba', y='Sr', style = FG_index, hue = "Popu
 plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), ncol=1)
 
 # general title
-plt.suptitle("Fine-Grained Fiamme Glass", fontsize=15, fontweight=0, color='black')
+plt.suptitle("Fine-Grained (FG) Fiamme Glass", fontsize=15, fontweight=0, color='black')
 
 # set size of plot
 sns.set_context("poster")
