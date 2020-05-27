1898b260a0efa7065116c2abc51536e787b74d2e
gennachiaro <gennachiaro@gmail.com>
2020-05-27 17:07:02 -0500

    ok

--- q2f-filtered/hsr-q2f-filtered.py
+++ q2f-filtered/hsr-q2f-filtered.py
@@ -11,12 +11,12 @@ import seaborn as sns; sns.set()
 import matplotlib.pyplot as plt
 
 #import csv file
-df = pd.read_csv('/Users/gennachiaro/Documents/vanderbilt/research/ora caldera/trace-elements/TraceElements_All_TEMP.csv', index_col=1)
+df = pd.read_csv('/Users/gennachiaro/Documents/vanderbilt/research/ora caldera/trace-elements/TraceElements_All.csv', index_col=1)
 
 #FGCP = df.loc[['ORA-2A-002_Type1','ORA-2A-002_Type2','ORA-2A-002','ORA-2A-003','ORA-2A-016_Type1','ORA-2A-016-Type2','ORA-2A-016-Type3','ORA-2A-016-Type4','ORA-2A-023','ORA-2A-024','MINGLED1-ORA-2A-024','MINGLED2-ORA-2A-024','MINGLED3-ORA-2A-024']]
 #FGCP_index = FGCP.index
 
-MG = df.loc[['ORA-2A-001','ORA-2A-005','ORA-2A-035','ORA-2A-036', 'ORA-2A-040']]
+MG = df.loc[['ORA-2A-001','ORA-2A-005','ORA-2A-035', 'ORA-2A-040']]
 MG_index = MG.index
 
 VCCR = df.loc[['ORA-5B-404A','ORA-5B-404B','ORA-5B-406','ORA-5B-408-SITE2','ORA-5B-408-SITE7','ORA-5B-408-SITE8','ORA-5B-411', 'ORA-5B-416']]
