b57fe00e217a60c60640fc7473c56387116e9833
gennachiaro <gennachiaro@gmail.com>
2021-01-21 16:04:44 -0600

    ok

--- ree+trace_hsr_mg+vccr.py
+++ ree+trace_hsr_mg+vccr.py
@@ -15,8 +15,10 @@ tr = pd.read_csv('/Users/gennachiaro/Documents/vanderbilt/research/ora caldera/t
 
 #FGCP = df.loc[['ORA-2A-002_Type1','ORA-2A-002_Type2','ORA-2A-002','ORA-2A-003','ORA-2A-016_Type1','ORA-2A-016-Type2','ORA-2A-016-Type3','ORA-2A-016-Type4','ORA-2A-023','ORA-2A-024','MINGLED1-ORA-2A-024','MINGLED2-ORA-2A-024','MINGLED3-ORA-2A-024']]
 #MG = df.loc[['ORA-2A-001','ORA-2A-005','ORA-2A-018','ORA-2A-031','ORA-2A-032','ORA-2A-035','ORA-2A-040']]
-VCCR = tr.loc [['ORA-5B-402','ORA-5B-404A','ORA-5B-406','ORA-5B-409','ORA-5B-411','ORA-5B-415','ORA-5B-416','ORA-5B-417']]
-#FG = df.loc [['ORA-5B-410','ORA-5B-412A-FG','ORA-5B-412B-FG','ORA-5B-414-FG']]
+
+#VCCR = tr.loc [['ORA-5B-402','ORA-5B-404A','ORA-5B-406','ORA-5B-409','ORA-5B-411','ORA-5B-415','ORA-5B-416','ORA-5B-417']]
+VCCR = tr.loc [['ORA-5B-404A','ORA-5B-404B','ORA-5B-405','ORA-5B-406','ORA-5B-408-SITE7','ORA-5B-408-SITE8','ORA-5B-411','ORA-5B-416']]
+
 VCCR_index = VCCR.index
 
 MG = tr.loc[['ORA-2A-001','ORA-2A-005','ORA-2A-018','ORA-2A-031','ORA-2A-032','ORA-2A-035','ORA-2A-040']]
@@ -24,7 +26,10 @@ MG_index = MG.index
 
 #import csv file
 REE = pd.read_csv('/Users/gennachiaro/Documents/vanderbilt/research/ora caldera/trace-elements/rare-earth-elements/All_REE_Normalized.csv', index_col=0)
-VCCRREE = REE.loc[['ORA-5B-402','ORA-5B-404A','ORA-5B-404B','ORA-5B-405','ORA-5B-406','ORA-5B-407','ORA-5B-408-SITE2','ORA-5B-408-SITE7','ORA-5B-408-SITE8','ORA-5B-409','ORA-5B-411','ORA-5B-412A-CG','ORA-5B-412B-CG','ORA-5B-413','ORA-5B-414-CG','ORA-5B-415','ORA-5B-416','ORA-5B-417']]
+#VCCRREE = REE.loc[['ORA-5B-402','ORA-5B-404A','ORA-5B-404B','ORA-5B-405','ORA-5B-406','ORA-5B-407','ORA-5B-408-SITE2','ORA-5B-408-SITE7','ORA-5B-408-SITE8','ORA-5B-409','ORA-5B-411','ORA-5B-412A-CG','ORA-5B-412B-CG','ORA-5B-413','ORA-5B-414-CG','ORA-5B-415','ORA-5B-416','ORA-5B-417']]
+VCCRREE = REE.loc [['ORA-5B-404A','ORA-5B-404B','ORA-5B-405','ORA-5B-406','ORA-5B-408-SITE7','ORA-5B-408-SITE8','ORA-5B-411','ORA-5B-416']]
+
+
 MGREE = REE.loc[['ORA-2A-001','ORA-2A-005','ORA-2A-018','ORA-2A-031','ORA-2A-032','ORA-2A-035','ORA-2A-040']]
 
 
@@ -35,17 +40,23 @@ sns.set_style("darkgrid")
 fig = plt.figure(figsize=(10,3.5))
 
 #group plot title
-title = fig.suptitle("High-Silica Rhyolite (MG + VCCR) Fiamme Glass", fontsize=12, y = 1.02)
+title = fig.suptitle("High-Silica Rhyolite (VCCR + MG) Fiamme Glass", fontsize=16, y = 1.03)
 
 #plot 1 
 
 #create ree plot
 plt.subplot(1,2,1)
 plot = sns.lineplot(data = MGREE, x= 'REE', y='Sample/Chondrite', hue = 'Population', sort = False, palette="Blues_d",legend="brief")
-plot1 = sns.lineplot(data = VCCRREE, x= 'REE', y='Sample/Chondrite', hue = 'Population', sort = False, palette="PuRd_d", hue_order = ['VCCR 1', 'VCCR 2', 'VCCR 3'])
+plot1 = sns.lineplot(data = VCCRREE, x= 'REE', y='Sample/Chondrite', hue = 'Population', sort = False, palette="PuRd_r", hue_order = ['VCCR 1', 'VCCR 2', 'VCCR 3'])
 
 #set location of legend
 plt.legend(loc='lower right')
+
+h,l = plot.get_legend_handles_labels()
+#plot just populations
+plt.legend(h[1:4]+h[5:9],l[1:4]+l[5:9],loc='lower right')
+
+
 plt.ylabel=("Sample/Chondrite")
 plt.ylim(0.05, 200)
 
@@ -55,7 +66,7 @@ plot1.set(yscale='log')
 #plot 2
 plt.subplot(1,2,2)
 #create trace element plot
-plot2 = sns.scatterplot(data = VCCR, x= 'Ba', y='Sr',hue = "Population", palette="PuRd_d", marker = '^', edgecolor="black", s=150, style = VCCR_index, alpha = 0.5, hue_order = ['VCCR 1', 'VCCR 2', 'VCCR 3'], legend = False)
+plot2 = sns.scatterplot(data = VCCR, x= 'Ba', y='Sr',hue = "Population", palette="PuRd_r", marker = '^', edgecolor="black", s=150, style = VCCR_index, alpha = 0.5, hue_order = ['VCCR 1', 'VCCR 2', 'VCCR 3'], legend = False)
 plot = sns.scatterplot(data = MG, x= 'Ba', y='Sr',hue = "Population" , palette="Blues_d",marker = 's', edgecolor="black", s=150, alpha = 0.5,style = MG_index, legend = False)
 
 #set location of legend
